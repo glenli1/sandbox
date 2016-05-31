63d2641799cba4dab29fb0ff1257a026837dec94
glenli1 <glenli1@gmail.com>
2016-05-31 13:10:42 -0400

    test local commit of VS project

---
+++ SandboxWebApp/SandboxWebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SandboxWebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SandboxWebApp/SandboxWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SandboxWebApp.MvcApplication" Language="C#" %>
